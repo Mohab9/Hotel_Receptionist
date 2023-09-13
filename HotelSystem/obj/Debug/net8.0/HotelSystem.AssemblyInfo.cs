8585756285fa2e7cbfc6d389d0df00cc2b919216
Mohab Ashraf <mohabmohab160@gmail.com>
2023-09-13 17:19:05 +0200

    make new Theme

--- HotelSystem/obj/Debug/net8.0/HotelSystem.AssemblyInfo.cs
+++ HotelSystem/obj/Debug/net8.0/HotelSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057b33577119bbd2ffffd4f2e53dd978f45f5401")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
